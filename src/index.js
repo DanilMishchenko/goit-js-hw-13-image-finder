8b776a090a555dac7d54566e9eaa9eb109382769
Danil Mishchenko <dan5679mi@gmail.com>
2021-06-06 12:30:41 +0300

    Стартовая разметка

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import './sass/main.scss';
+// import API from './js/apiService.js';
---
+++ src/js/apiService.js
@@ -0,0 +1,3 @@
+const API_KEY = '21952569-4b44b40a7ae5579ea0d6f7f48';
+const BASE_URL = 'https://pixabay.com/api/?image_type=photo';
+
